19b5fdb1bc1d16a373fafa6e80a055f9f5e51e76
jankrum <professionalninety@gmail.com>
2023-04-24 14:08:29 -0700

    Added bcrypt functionality to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,72 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
 const path = require("path");
-
-app.use(express.static(path.join(__dirname, "static")));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+app.use(express.json());
+
+async function getUserByUsername(username) {
+    const fakeDB = [{
+        "username": "jankrum",
+        "id": "123",
+        "passwordHash": "$2b$10$qKOX1T9Q9tc9cx/nGGgyxeIcciU03h.cryBBDXitHLJuTi6Q6xn3O"
+    }]
+
+    for (const user of fakeDB) {
+        if (user.username == username) {
+            return user;
+        }
+    }
+
+    return null;
+}
+
+
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Query the database to get the user's hashed password
+    const user = await getUserByUsername(username);
+    if (!user) {
+        console.log("\t-Username not found");
+        // The user doesn't exist - return a 401 Unauthorized response
+        return res.status(401).send('Invalid username or password');
+    }
+
+    // Compare the user's hashed password with the entered password
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!passwordMatch) {
+        console.log("\t-Password unmatch");
+        // The password is incorrect - return a 401 Unauthorized response
+        return res.status(401).send('Invalid username or password');
+    }
+
+    // Generate a session token for the user
+    //const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    // Set the session token as an HTTP-only cookie
+    //res.cookie('sessionToken', token, { httpOnly: true });
+
+    // Return a 200 OK response
+    res.status(200).end();
+});
+
+
 app.get("/login", (req, res) => {
+    console.log("Login request");
+    res.render("login");
+})
+
+app.get("/dashboard", (req, res) => {
+    res.send("YOURE IN");
+})
+
+app.get("/", (req, res) => {
     console.log("Login request");
     res.render("login");
 })
---
+++ Backend/public/login.js
@@ -0,0 +1,25 @@
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    const response = await fetch('/api/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        // The login was successful - redirect to the user's dashboard
+        window.location.href = '/dashboard';
+    } else {
+        // The login was unsuccessful - display an error message
+        const errorMessage = await response.text();
+        const errorDiv = document.querySelector('#error');
+        errorDiv.textContent = errorMessage;
+    }
+});
