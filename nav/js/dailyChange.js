82a059385af58202196964c8cef64e723e7ef711
John <bf4ltc@gmail.com>
2023-05-12 13:20:11 -0700

    dailyChange.js introduced

---
+++ nav/js/dailyChange.js
@@ -0,0 +1,24 @@
+const confirmForm = document.querySelector("#confirm");
+var morning = document.getElementById("A");
+var afternoon = document.getElementById("B");
+
+confirmForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(morning.value);
+    console.log(afternoon.value);
+
+
+    if (morning.value == "0" || afternoon.value == "0") {
+        window.alert("Please select a location for your morning or afternoon")
+    } else {
+        // const formData = new FormData(confirmForm);
+        // const morning = formData.get("morning");
+        // const afternoon = formData.get("afternoon")
+
+        // const response = await fetch('/api/login', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ username, password }),
+        //     headers: { 'Content-Type': 'application/json' }
+        // }
+    }
+})
